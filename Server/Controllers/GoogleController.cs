a6bac8da94cf0a3c47623974cb406e727b8a448b
Malkaberry <84181559+Malkaberry@users.noreply.github.com>
2024-04-29 13:57:38 +0300

    ניסיון הוספת יוזר

--- Server/Controllers/GoogleController.cs
+++ Server/Controllers/GoogleController.cs
@@ -1,40 +1,44 @@
-﻿using CountOnIt.Server.Models;
+﻿using CountOnIt.Shared.Models.present.toAdd;
+using CountOnIt.Shared.Models.present.toShow;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using TriangleDbRepository;
-using static System.Net.WebRequestMethods;
 
-namespace CountOnIt.Server.Controllers
+[Route("api/[controller]")]
+[ApiController]
+public class GoogleController : ControllerBase
 {
-    [Authorize]
-    [ApiController]
-    [Route("[controller]")]
-    public class GoogleController : ControllerBase
+
+    private readonly DbRepository _db;
+    public GoogleController(DbRepository db)
     {
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly DbRepository _db;
-
-        public GoogleController(UserManager<ApplicationUser> userManager)
-        {
-            _userManager = userManager;
-        }
-
-        public GoogleController(DbRepository db)
-        {
-            _db = db;
-        }
-
-        [HttpGet("GetUserId")]
-        public async Task<IActionResult> GetUserId()
-        {
-            var user = await _userManager.GetUserAsync(HttpContext.User);
-            if (user != null)
-            {
-                return Ok(user.Id);
-            }
-            return NotFound();
-        }     
+        _db = db;
     }
+
+    [HttpPost("Adduser/{userGoogleID}")] // יצירת משתמש חזש
+    public async Task<IActionResult> Adduser(string userGoogleID, UserToAdd userToAdd)
+    {
+        object userToAddParam = new
+        {
+            googleID = userGoogleID,
+            firstName = userToAdd.firstName,
+            lastName = userToAdd.lastName
+
+        };
+
+        string insertUserQuery = "INSERT INTO users (googleID,firstName, lastName) values (@googleID ,@firstName ,@lastName)";
+
+        int newUserId = await _db.InsertReturnId(insertUserQuery, userToAddParam);
+
+        if (newUserId != 0)
+        {
+            return Ok();
+        }
+
+        return BadRequest("user not created");
+    }
+
+
 }
---
+++ Shared/Models/present/toAdd/UserToAdd.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountOnIt.Shared.Models.present.toAdd
+{
+    public class UserToAdd
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
